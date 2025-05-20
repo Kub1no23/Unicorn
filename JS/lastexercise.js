211c8b51430dbbe1c214d54b09c989c90de2fa2f
Jakub Zlámal <kub1no23@100HPJakoubek.>
2025-05-20 14:04:52 +0200

    callBacks promises and await

--- JS/lastexercise.js
+++ JS/lastexercise.js
@@ -14,3 +14,38 @@ button.addEventListener('click', async () => {
     const message = await fetchMessage();
     text.innerText = message;
 });
+
+// function fetchMessage(callBack) {
+//     setTimeout(() => {
+//         callBack('Hello from the future!');
+//     }, 2000);
+// }
+
+// const button = document.querySelector('button');
+// const text = document.querySelector('p');
+
+// button.addEventListener('click', () => {
+//     text.innerText = 'Loading...'
+//     let message;
+//     fetchMessage((message) => {
+//         text.innerText = message;
+//     })
+// });
+
+// function fetchMessage() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve("Hello from the future!");
+//         }, 2000);
+//     });
+// }
+
+// const button = document.querySelector('button');
+// const text = document.querySelector('p');
+
+// button.addEventListener('click', async () => {
+//     text.innerText = 'Loading...'
+//     fetchMessage().then((message) => {
+//         text.innerText = message;
+//     });
+// });
